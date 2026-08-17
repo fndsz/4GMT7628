d8e8d3dfe5f25af54d131220511f3de2aa8d7d17
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-18 02:10:32 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -3,7 +3,7 @@
 # MMDVM-OPENWRT
 
 # 修改默认IP
-sed -i 's/192.168.6.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名称
 sed -i 's/ImmortalWrt/Ostar/g' package/base-files/files/bin/config_generate
