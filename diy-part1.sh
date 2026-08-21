457bd93de76024d8e4496d9991acb23e40424a5e
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-22 04:55:29 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,3 +17,5 @@
 git clone https://github.com/fndsz/mmdvm-op.git package/mmdvm/mmdvm-openwrt
 
 git clone https://github.com/lazywalker/devtools-feeds.git package/led/devtools
+
+git clone https://github.com/fndsz/luci-app-oled.git package/luci-app-oled
