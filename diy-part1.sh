9f8cba6a348a0cf95cf40dfba3d6a6416e48778b
fndsz <45010248+fndsz@users.noreply.github.com>
2026-08-28 09:30:51 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com># 版权所有 © 2019-2020 P3TERX <https://p3terx.com># 版权所有 © 2019-2020 P3TERX <https://p3terx.com>
 #
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
+# This is free software, licensed under the MIT License.# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 这是自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 这是自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 这是自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 这是自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 这是自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 这是自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 这是自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。# 本软件为自由软件，采用 MIT 许可证。
+# See /LICENSE for more information.# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。# 有关更多信息，请参阅 /LICENSE。
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part1.sh
-# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# File name: diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh# 文件名：diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）# 描述：OpenWrt DIY 脚本 第一部分（更新 feed 之前）
 #
 
-# Uncomment a feed source
+# Uncomment a feed source# 取消注释一个 feed 源# 取消注释一个 feed 源# 取消注释一个 feed 源
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# Add a feed source
+# Add a feed source# 添加信息源# 添加信息源# 添加信息源# 添加信息源# 添加信息源# 添加信息源# 添加信息源# 添加信息源# 添加信息源# 添加信息源# 添加信息源# 添加信息源# 添加信息源# 添加信息源# 添加信息源
 git clone https://github.com/fndsz/mmdvm-op.git package/mmdvm/mmdvm-openwrt
 
 git clone https://github.com/lazywalker/devtools-feeds.git package/led/devtools
+
+git clone https://github.com/Kazagumo/luci-app-ssd1306.git package/luci-app-ssd1306
